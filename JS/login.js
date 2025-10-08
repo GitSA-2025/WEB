1941e2a38783ca634a7d8813c83d3f3209a42d3f
GitSA-2025 <accesssystemfatec@gmail.com>
2025-10-08 17:48:48 -0300

    Atualizar o login.js

--- JS/login.js
+++ JS/login.js
@@ -3,7 +3,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
   const email = document.getElementById("useremail").value;
   const senha = document.getElementById("userpass").value;
 
-  const res = await fetch("http://localhost:3000/api/login", {
+  const res = await fetch("https://apiwebmobile-production.up.railway.app/api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, senha })
